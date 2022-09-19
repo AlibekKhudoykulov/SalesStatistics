f019846b96d278c57efbc693df3bf5f0ea7b1742
Alibek_Khudoykulov <alibek_khudoykulov@epam.com>
2022-09-19 17:11:48 +0500

    Project is created

---
+++ src/main/java/com/example/salesstatistics/SalesStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesStatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesStatisticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salesstatistics/SalesStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesStatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
